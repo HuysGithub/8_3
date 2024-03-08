1be241c96858f21d4f0e7576e43012c3fe3c9079
HuysGithub <august16.hn@gmail.com>
2024-03-08 10:31:42 +0700

    beautiful

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-onload = () =>{
+window.onload = () => {
         document.body.classList.remove("container");
-};
+    
+        setTimeout(function() {
+            var leftImg = document.querySelector('.left-img');
+            var rightImg = document.querySelector('.right-img');
+            
+            leftImg.classList.add('show');
+            rightImg.classList.add('show');
+        }, 3000);
+    };
+    
